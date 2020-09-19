58f1660e1fc185a21549aae7dcf2902447b906ff
AleksandraMaa <tengma@berkeley.edu>
2020-09-18 17:12:34 -0700

    Temp save

--- code/shiny_app/ui.R
+++ code/shiny_app/ui.R
@@ -10,7 +10,15 @@ library(plotly)
 # not sure if this makes a difference
 knitr::opts_knit$set(root.dir = here())
 
+costs_temp_india <- 1
+costs_temp_kenya <- 2
+costs_temp_nigeria <- 3
+costs_temp_vietnam <- 4
 
+prevalence_india <- 0.57
+prevalence_kenya <- 0.35
+prevalence_nigeria <- 0.27
+prevalence_vietnam <- 0.15
 
 nsims <- 1e2
 
@@ -78,17 +86,17 @@ shinyUI(
                             #                      list("india", "kenya", "nigeria", "vietnam"), 
                             #                    selected = list("india", "kenya", "nigeria", "vietnam")  ), 
                             helpText("For reference:", br(),
-                                     "Unit costs in India is *insert india*", br(),
-                                     "Unit costs in Kenya is *insert kenya*", br(), 
-                                     "Unit costs in Nigeria is *insert nigeria*", br(), 
-                                     "Unit costs in Vietnam is *insert vietnam*"),
+                                     paste("Unit costs in India is", costs_temp_india), br(),
+                                     paste("Unit costs in Kenya is", costs_temp_kenya), br(), 
+                                     paste("Unit costs in Nigeria is", costs_temp_nigeria), br(), 
+                                     paste("Unit costs in Vietnam is", costs_temp_vietnam)),
                             numericInput("param37", label = h3("Prevalence in the new region"), value = round(prevalence_r_in,2)),
                             
                             helpText("For reference:", br(),
-                                     "Prevalence in India is 0.57", br(),
-                                     "Prevalence in Kenya is 0.35", br(), 
-                                     "Prevalence in Nigeria is 0.27", br(), 
-                                     "Prevalence in Vietnam is 0.15")
+                                     paste("Prevalence in India is", prevalence_india), br(),
+                                     paste("Prevalence in Kenya is", prevalence_kenya), br(), 
+                                     paste("Prevalence in Nigeria is", prevalence_nigeria), br(), 
+                                     paste("Prevalence in Vietnam is", prevalence_vietnam))
                    )
                  ),
                  mainPanel(
