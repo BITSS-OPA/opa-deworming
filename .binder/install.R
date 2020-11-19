eb9d127d95aedd834f8f3c6d01f8f9f977c97fc9
AleksandraMaa <tengma@berkeley.edu>
2020-11-18 20:34:02 -0800

    updates to install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,21 +1,24 @@
-install.packages(
-  "ggplot2",
-  "gh",
-  "glue",
-  "haven",
-  "here",
-  "hrbrthemes",
-  "kableExtra",
-  "knitr",
-  "plotly",
-  "readr",
-  "readxl",
-  "rmarkdown",
-  "rootSolve",
-  "rsconnect",
-  "shiny",
-  "shinyBS",
-  "shinyjs",
-  "shinythemes",
-  "tidyverse"
-)
+# packages
+
+install.packages("ggplot2")
+install.packages("gh")
+install.packages("glue")
+install.packages("haven")
+install.packages("here")
+install.packages("hrbrthemes")
+install.packages("kableExtra")
+install.packages("knitr")
+install.packages("plotly")
+install.packages("readr")
+install.packages("readxl")
+install.packages("rmarkdown")
+install.packages("rootSolve")
+install.packages("rsconnect")
+install.packages("shiny")
+install.packages("shinyBS")
+install.packages("shinyjs")
+install.packages("shinythemes")
+install.packages("tidyverse")
+
+
+
