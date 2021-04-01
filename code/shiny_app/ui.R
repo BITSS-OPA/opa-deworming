b41d063943887752e4f4f1a0b90669be0e81f5b6
Peter Zhang <petejzh@gmail.com>
2021-04-01 13:02:26 -0700

    update opa logos

--- code/shiny_app/ui.R
+++ code/shiny_app/ui.R
@@ -191,7 +191,8 @@ shinyUI(
                  sidebarPanel(
                    div( id = "KA",
                         
-                        fluidRow(column(
+                        fluidRow(column(3),
+                                 column(
                           6,
                           align = "center",
                           tags$a(
@@ -315,7 +316,8 @@ shinyUI(
                  sidebarPanel(
                    div(id = "All",
                        
-                       fluidRow(column(
+                       fluidRow(column(3),
+                                column(
                          6,
                          align = "center",
                          tags$a(
@@ -1269,20 +1271,7 @@ shinyUI(
                             style = "position:relative; padding-top:10px;",
                             downloadButton("downloadParams", "Output Parameters"),
                             downloadButton("downloadPlotAll", "Save Plot")
-                            ),
-                   
-                   fluidRow(column(
-                     10,
-                     align = "center",
-                     tags$a(
-                       img(
-                         src = "OPA_layers_assumptions.png",
-                         width = "70%",
-                         height = "auto"
-                       )
-                     )
-                     
-                   ))
+                            )
                    
                  )),
                  mainPanel(
